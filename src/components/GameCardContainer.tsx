2fc4486916139fc255ea8542eb6f54a6353cef69
MenaGrace <menamakrem79@gmail.com>
2025-07-18 22:51:36 +0300

    fetch game details

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -8,7 +8,14 @@ interface Props {
 const GameCardContainer = ({ children }: Props) => {
   return (
     <div>
-      <Box borderRadius={10} overflow={"hidden"}>
+      <Box
+        _hover={{
+          transform: "scale(1.03)",
+          transition: "transform 0.15s ease-in",
+        }}
+        borderRadius={10}
+        overflow={"hidden"}
+      >
         {children}
       </Box>
     </div>
